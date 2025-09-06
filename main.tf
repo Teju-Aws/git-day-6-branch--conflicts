29b5d228e2d97028205d33f764cab915e1c1e0e6
Teju-Aws <tejaswinigundluru@gmail.com>
2025-09-07 02:41:10 +0530

    Re-commit changes after reset

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "name" {
     ami="ami-0d0ad8bb301edb745"
-    instance_type = "t4.micro"
+    instance_type = "t3.micro"
       #vpc_id = aws_vpc.name.id
       subnet_id = aws_subnet.name.id
       vpc_security_group_ids =[aws_security_group.name.id]
       tags ={
 
-        Name = "teju-Aws"
+        Name = "teju-Awssssssssss"
 
       }
 }
